98f7045afe70f64ea7f0b7e46fc0add5a7c09de6
Samed JALOUALI <samed.jalouali@siteflow.fr>
2020-04-14 15:28:36 +0200

    fix Content Security Policy add connect-src localhost and algolia url api

--- src/main/java/com/oce/app/config/SecurityConfiguration.java
+++ src/main/java/com/oce/app/config/SecurityConfiguration.java
@@ -63,7 +63,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .accessDeniedHandler(problemSupport)
         .and()
             .headers()
-            .contentSecurityPolicy("default-src 'self'; frame-src 'self' data:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://storage.googleapis.com; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:")
+            .contentSecurityPolicy("default-src 'self'; frame-src 'self' data:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://storage.googleapis.com; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data: ; connect-src http://localhost:8080 https://*.algolia.net https://*.algolianet.com")
         .and()
             .referrerPolicy(ReferrerPolicyHeaderWriter.ReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN)
         .and()
