9e6b412a42dc6a062c021fc48ee518a178be3f5a
Samed JALOUALI <samed.jalouali@siteflow.fr>
2020-04-15 16:49:13 +0200

    deploy one gast-tn

--- src/main/java/com/oce/app/config/SecurityConfiguration.java
+++ src/main/java/com/oce/app/config/SecurityConfiguration.java
@@ -63,7 +63,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .accessDeniedHandler(problemSupport)
         .and()
             .headers()
-            .contentSecurityPolicy("default-src 'self'; frame-src 'self' data:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://storage.googleapis.com; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data: ; connect-src http://localhost:8080 https://*.algolia.net https://*.algolianet.com")
+            .contentSecurityPolicy("default-src 'self'; frame-src 'self' data:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://storage.googleapis.com; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data: ; connect-src http://localhost:8080 http://localhost:9000 https://*.azurewebsites.net https://*.algolia.net https://*.algolianet.com")
         .and()
             .referrerPolicy(ReferrerPolicyHeaderWriter.ReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN)
         .and()
